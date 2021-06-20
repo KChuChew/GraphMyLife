406f9ce0eee17fc644f03292363784fd486ec778
Kevin Chu <kevin.git.chu@gmail.com>
2021-06-19 19:19:44 -0700

    Create ViewModels in Main Activity for scoping test

--- app/src/main/java/ca/tutorials/graphmylife/views/MainActivity.kt
+++ app/src/main/java/ca/tutorials/graphmylife/views/MainActivity.kt
@@ -5,11 +5,15 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.lifecycle.viewModelScope
 import ca.tutorials.graphmylife.databinding.ActivityMainBinding
+import ca.tutorials.graphmylife.viewmodels.ChartCategoryViewModel
+import ca.tutorials.graphmylife.viewmodels.CreateCategoryViewModel
 import ca.tutorials.graphmylife.viewmodels.MainActivityViewModel
 
 open class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val mainActivityViewModel : MainActivityViewModel by viewModels()
+    private val chartCategoryViewModel : ChartCategoryViewModel by viewModels()
+    private val createCategoryViewModel : CreateCategoryViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
