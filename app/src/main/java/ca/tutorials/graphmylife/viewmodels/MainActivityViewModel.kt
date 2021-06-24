339eb9050ab2cfe6c8813c34b0e89f30b4f07825
Kevin Chu <kevin.git.chu@gmail.com>
2021-06-23 19:06:28 -0700

    Basic outline of binding in MainActivity

--- app/src/main/java/ca/tutorials/graphmylife/viewmodels/MainActivityViewModel.kt
+++ app/src/main/java/ca/tutorials/graphmylife/viewmodels/MainActivityViewModel.kt
@@ -6,4 +6,13 @@ import androidx.lifecycle.ViewModel
 
 class MainActivityViewModel : ViewModel() {
     var toolbarVisibility = MutableLiveData(View.VISIBLE)
+    var counter = MutableLiveData(0)
+
+    init {
+        counter.value = 0
+    }
+
+    fun navigationOnClick() {
+        counter.value = counter.value?.plus(1)
+    }
 }
